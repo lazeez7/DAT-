42fa00099b3756ed71cdea7de458715286b0a4c0
lazeez7 <153117025+lazeez7@users.noreply.github.com>
2024-03-14 13:17:57 +0500

    Add files via upload

---
+++ DAT internet magazind/js.js
@@ -0,0 +1,35 @@
+// let correctAnswer = 0;
+// let incorrectAnswer = 0;
+// let вопрос = prompt(' как поставить наш элемент по центру в css ')
+
+// if(вопрос == 'margin 0 auto'){
+//     alert('Правильно')
+//     correctAnswer ++
+// } else{
+//     alert('пошёл вон из академии ')
+//     incorrectAnswer ++
+// }
+
+// let вопрос2 = prompt(' Что больше h1 или h2?')
+
+// if(вопрос2 == 'h1'){
+//     alert('Правильно')
+//     correctAnswer ++
+// } else{
+//     alert('пошёл вон из академии ')
+//     incorrectAnswer ++
+// }
+
+// let вопрос3 = prompt(' что означает шифровка HTML ')
+
+// if(вопрос3 == 'HyperText Markup Language'){
+//     alert('Правильно')
+//     correctAnswer ++
+// }  else{
+//     alert('пошёл вон из академии ')
+//     incorrectAnswer ++
+// }
+// alert(`правильный ответы: ${correctAnswer}, неправильный ответы ${incorrectAnswer}`)
+
+
+
